0df896160cfd024f2664d92f7cb083017cd1bb60
weiyuanlong@quickbusiness.cn <weiyuanlong@quickbusiness.cn>
2019-07-10 17:32:43 +0800

    mybatis逆向工程

---
+++ src/main/java/com/GeneratorSqlmap.java
@@ -0,0 +1,44 @@
+package com;
+
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * mybatis逆向工程
+ * 
+ * @author 魏元龙 15012710712
+ * @version 1.0 , 2018-05-14 23:07:13
+ *
+ */
+public class GeneratorSqlmap {
+
+	public void generator() throws Exception{
+
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		//指定 逆向工程配置文件
+		File configFile = new File("generatorConfig.xml"); 
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+	} 
+	public static void main(String[] args) throws Exception {
+		try {
+			GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+			generatorSqlmap.generator();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
